4d52f007c1baa69049fb52fa4bd205604dcf824e
OtsukaLabgithub <124670056+OtsukaLabgithub@users.noreply.github.com>
2023-03-10 16:20:43 +0900

    add heredoc func

--- srcs/heredoc/heredoc.c
+++ srcs/heredoc/heredoc.c
@@ -11,6 +11,12 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+typedef strcut s_h t_h;
+
+strcut s_h{
+	char *e;
+	t_h *next;
+}
 
 bool	is_heredoc(t_line *line)
 {
@@ -56,11 +62,51 @@ int	open_heredocfile(void)
 	return (fd);
 }
 
-int	heredoc_start(t_line *line)
+t_h  *new_heredoc(char *e)
 {
-	int	fd;
+	t_h *h;
 
-	fd = open_heredocfile();
+	h = calloc(sizeof(t_h),1);
+	if(!h)
+		_err("malloc");
+	h->e = strdup("e");
+	if(!h->e)
+		_err("malloc");
+	h->next = NULL;
+	return h;
+}
+
+t_h *heredoc_last(t_h *head)
+{
+	while(1)
+	{
+		if(head->next == NULL)
+			break;
+		head = head->next;
+	}
+	return head;
+}
+
+void heredoc_addback(t_h **head,t_h *new)
+{
+	t_h *last;
+
+	if(head)
+	{
+		if(*head)
+		{
+			last = heredoc_last(*head);
+			last->next = new;
+		}
+		else
+			*head = new;
+	}
+}
+
+void	heredoc_eof(t_line *line,t_h *head)
+{
+	t_h *new;
+
 	while (1)
 	{
 		if (line->type == T_EOF_R)
@@ -68,11 +114,49 @@ int	heredoc_start(t_line *line)
 		if (is_heredoc(line))
 		{
 			line = line->next;
+			new = new_heredoc(line->token->word);
+			heredoc_addback(&head,new);
 		}
 		line = line->next;
 	}
 }
+
+void _err_heredoc(char *m)
+{
+	printf("%s: warning: here-document at line 3 delimited by end-of-file (wanted `EOF')\n",m);
+}
+
+void	heredoc_start(int fd,t_h *head)
+{
+	char *line;
+	while(1)
+	{
+		line = readline(">");
+		if(line == NULL)
+			_err_heredoc("minishell");
+		if(strncmp(head->e,line) == 0)
+		{
+			if(head->next == NULL)
+				break;
+			else
+				head = head->next;
+		}
+		dprintf(fd, "%s\n", line);
+	}
+
+}
+
 int	heredoc(t_node *node)
 {
-	heredoc_start(node->line);
+	int	fd;
+	t_h *head;
+
+	head = NULL;
+	heredoc_eof(node->line,head);
+	if(head ==NULL)
+		return 0;
+	fd = open_heredocfile();
+	heredoc_start(fd,head);
+	dup2(fd,0);
+	close(fd);
 }
