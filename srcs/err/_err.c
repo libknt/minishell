9b447fa141dca45c8c9323f8c81f38261ed5b858
kentaro yoda <101494707+libknt@users.noreply.github.com>
2023-03-25 16:31:15 +0900

    Update _err.c
    add void	_err_minishell(char *m)
    {
    	global.exit_status = 1;
    	ft_putstr_fd("minishell: ", STDERR_FILENO);
    	ft_putendl_fd(m, STDERR_FILENO);
    }

--- srcs/err/_err.c
+++ srcs/err/_err.c
@@ -114,6 +114,13 @@ void	_err_cmd_not_found(char *m)
 	global.exit_status = 127;
 }
 
+void	_err_minishell(char *m)
+{
+	global.exit_status = 1;
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
+	ft_putendl_fd(m, STDERR_FILENO);
+}
+
 /*
 
 void	_err_syntax(char *mes)
