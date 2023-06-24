daf4331a68976d40a1f6734db9706fcf02e61a44
MasaDevs <110250805+MasaDevs@users.noreply.github.com>
2023-06-24 21:07:07 +0900

    norm unset

--- srcs/buildin/unset.c
+++ srcs/buildin/unset.c
@@ -3,19 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: masahitoarai <masahitoarai@student.42.f    +#+  +:+       +#+        */
+/*   By: marai <marai@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 15:36:46 by marai             #+#    #+#             */
-/*   Updated: 2023/06/24 14:02:24 by marai            ###   ########.fr       */
+/*   Updated: 2023/06/24 21:05:37 by marai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	unset_env(char *arg, t_env **env)
+{
+	t_env	*env_node;
+
+	env_node = *env;
+	while (env_node)
+	{
+		if (!ft_strcmp(arg, env_node->key))
+		{
+			if (env_node->prev)
+				env_node->prev->next = env_node->next;
+			else
+				*env = env_node->next;
+			if (env_node->next)
+				env_node->next->prev = env_node->prev;
+			free(env_node->key);
+			free(env_node->value);
+			free(env_node);
+			break ;
+		}
+		env_node = env_node->next;
+	}
+}
+
 void	unset(char *argv[], t_env **env, t_status *s)
 {
 	ssize_t	i;
-	t_env	*env_node;
 
 	if (!argv)
 		return ;
@@ -27,30 +50,11 @@ void	unset(char *argv[], t_env **env, t_status *s)
 		ft_putendl_fd("unset: not enough arguments", 2);
 		s->status = 1;
 	}
-
 	while (argv[i])
 	{
 		if (!check_env_vari(argv[i]))
 			s->status = 1;
-		env_node = *env;
-		while (env_node)
-		{
-			if (!ft_strcmp(argv[i], env_node->key))
-			{
-				if (env_node->prev)
-					env_node->prev->next = env_node->next;
-				else
-					*env = env_node->next;
-
-				if (env_node->next)
-					env_node->next->prev = env_node->prev;
-				free(env_node->key);
-				free(env_node->value);
-				free(env_node);
-				break ;
-			}
-			env_node = env_node->next;
-		}
+		unset_env(argv[i], env);
 		i++;
 	}
 }
