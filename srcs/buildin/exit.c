68ca04cd90a21e0357cdc2de714d8c35eba05f67
key-door <101494707+key-door@users.noreply.github.com>
2023-06-24 21:18:48 +0900

    add

--- srcs/buildin/exit.c
+++ srcs/buildin/exit.c
@@ -6,7 +6,7 @@
 /*   By: kyoda <kyoda@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 22:45:28 by keys              #+#    #+#             */
-/*   Updated: 2023/06/24 21:11:10 by kyoda            ###   ########.fr       */
+/*   Updated: 2023/06/24 21:18:39 by kyoda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,20 @@ extern t_global	g_global;
 
 void check_sign(char *str)
 {
-	if((str[0] == '-') || (str[0] == '+' ) )
+	if(str[0])
 	{
-		if(str[1])
+		if((str[0] == '-') || (str[0] == '+' ) )
 		{
-			if((str[1] == '-') || (str[1] == '+' ) )
+			if(str[1])
 			{
-				ft_putendl_fd("exit", STDERR_FILENO);
-				ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
-				ft_putstr_fd(str, STDERR_FILENO);
-				ft_putendl_fd(": numeric argument required", STDERR_FILENO);
-				exit(255);
+				if((str[1] == '-') || (str[1] == '+' ) )
+				{
+					ft_putendl_fd("exit", STDERR_FILENO);
+					ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+					ft_putstr_fd(str, STDERR_FILENO);
+					ft_putendl_fd(": numeric argument required", STDERR_FILENO);
+					exit(255);
+				}
 			}
 		}
 	}
