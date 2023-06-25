8420973512605c736e8b43cf07e552cd2fe5ffdd
key-door <101494707+key-door@users.noreply.github.com>
2023-06-25 14:31:25 +0900

    add comment

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: kyoda <kyoda@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 13:54:10 by keys              #+#    #+#             */
-/*   Updated: 2023/06/25 13:04:35 by kyoda            ###   ########.fr       */
+/*   Updated: 2023/06/25 14:31:02 by kyoda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,3 +74,53 @@ int	main(int argc, char **argv, char **envp)
 	free_env(env);
 	return (g_global.exit_status);
 }
+
+
+/**
+ * bash
+ * 	-コマンドを入力して、解析して、実行する。
+ * lexer
+ * 	-コマンドの解析
+ * 		-単語分割
+ * 			- echo -e -n hello    echo | -e | -n | hello
+ *		-error { || && ;; |& & ; ( ) | \n "" ''  } 
+ * expand
+ * 	- 変数展開
+ * 		-{ 環境変数　”” ’’ } の展開
+ * parser
+ *  -コマンドの解析
+ * 		-構文解析　 => 再起下降構文解析木
+ * 			- コマンド　パイプ　リダイレクト　heredoc
+ * 		- error { [ command || command ],  [リダイレクト] }
+ * exe
+ * 	-リダイレクト、heredocの実行
+ * 	-コマンドの実行
+ * 		- [ echo -e -n hello ] => echoのpathを探す  /bin/echo
+ * 		-error 
+ * 			[ 'ファイルがない' 'ファイルの権限がない' 'コマンドがない' ... ]
+ * 		-buildin
+ * 			-echo
+ * 				-echo hello
+ * 					-引数に与えられた文字列を標準出力に出力する
+ * 				- echo -n hello
+ * 					- [ -n ]
+ * 						-改行なしで出力する
+ * 			-pwd
+ * 				-カレントディレクトリを表示する
+ * 			-cd
+ * 				-cd [path]
+ * 				-カレントディレクトリを変更する
+ * 			-env
+ * 				-環境変数(valueのあるもの)を出力する
+ * 			-export
+ * 				- export
+ * 					-環境変数(valueのないものも含める)を昇順で出力する
+ * 				- export [key || key=value]
+ * 					- 環境変数を更新する
+ * 			-unset
+ * 				-unset [環境変数(key)]
+ * 				-設定された環境変数を削除する
+ * 			-exit
+ * 				-exit [終了ステータス]
+ * 				- プロセスを終了する
+ */
